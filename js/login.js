a3f46093580d1e325d3a2677f983763cfea2e8b1
itCanelita <abelastacio86@gmail.com>
2024-03-07 19:33:13 -0400

    Estructura General del proyecto

---
+++ js/login.js
@@ -0,0 +1,12 @@
+const btnIniciar = document.querySelector("#btn-iniciar")
+const icono = document.querySelector("#icono")
+const login = document.querySelector("#login")
+const alert = document.querySelector("#alert")
+
+btnIniciar.addEventListener('click', ()=> {
+    login.classList.remove("error")
+    alert.classList.remove("alertA")
+    void login.offsetWidth;
+    login.classList.add("error")
+    alert.classList.add("alertA")
+})
